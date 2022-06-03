e41a4a66f9eabb2ea3349692c96aa7ea519a110f
Md. Mohacel Hosen <96243118+M0HACEL@users.noreply.github.com>
2022-06-03 12:32:45 +0600

    Update is_leap_year.js
    if the year is divisible by 400 and 4 then the year is "Leap Year"  other wise it's not a leap year

--- is_leap_year.js
+++ is_leap_year.js
@@ -6,3 +6,16 @@ function is_leap_year(year){
     }
 }
 is_leap_year(1700);
+
+function leap_year(year){
+    if(year%400==0){
+        console.log(year+" is leap year");
+    }else if(year%100==0){
+        console.log(year+" is not leap year");
+    }else if(year%4==0){
+        console.log(year+" is leap year");
+    }else{
+        console.log(year+" is not leap year");
+    }
+}
+leap_year(1700)
